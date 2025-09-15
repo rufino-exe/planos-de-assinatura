57bf459973507ccca10b367b00849776b3fda17f
NewVnNessa <nessa.vila@outlook.com>
2025-09-15 19:59:34 -0300

    Continuação

--- POO/src/PlanosAssinatura/PlanosAssinatura.java
+++ POO/src/PlanosAssinatura/PlanosAssinatura.java
@@ -1,5 +1,35 @@
 package PlanosAssinatura;
 
 public class PlanosAssinatura {
-    
+
+    private String nome;
+    private Double precoMensal;
+
+    public PlanosAssinatura() {
+    }
+
+    public PlanosAssinatura(String nome, Double precoMensal) {
+        this.nome = nome;
+        this.precoMensal = precoMensal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPrecoMensal() {
+        return precoMensal;
+    }
+
+    public void setPrecoMensal(Double precoMensal) {
+        this.precoMensal = precoMensal;
+    }
+
+    public Double CalcularPrecoAnual() {
+        return precoMensal * 12;
+    } 
 }
